6d19e64274b25c855a980ad944a14bd9535fe55e
Denis Michailov <kamrad.reg@gmail.com>
2017-10-03 15:51:18 +1000

    initial

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import App from './App';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+
+import rootReducer from './root-reducer';
+import  { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
+registerServiceWorker();
---
+++ src/root-reducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+import cards from './reducers/cards-reducer';
+
+export default combineReducers({
+  // describe state here
+  cards
+})
